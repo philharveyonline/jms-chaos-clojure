1c86c86740f2bfd5d270890f5bffe27075df8266
Phil Harvey <phil@philharveyonline.com>
2013-08-03 23:14:26 +0100

    Now running arbitrary number of slightly-randomised producers in the background

--- src/org/apache/qpid/jmschaos/core.clj
+++ src/org/apache/qpid/jmschaos/core.clj
@@ -6,10 +6,12 @@
   (:require [clj-http.client :as client])
   (:use [clojure.contrib.java-utils]))
 
+
 (def queue-name "chaos-queue")
 
 (def queue-url (format "http://localhost:8080/rest/queue/default/%s" queue-name))
 (def binding-url (format "http://localhost:8080/rest/binding/default/amq.direct/%1$s/%1$s" queue-name))
+
 
 (defn create-queue []
   (println "About to create queue")
@@ -22,6 +24,28 @@
   (client/delete queue-url)
   (println "Deleted queue"))
   
+(defn run-consumer [session queue]
+  (future
+    (let [consumer (.createConsumer session queue)]
+      (.receive consumer 1000))))
+
+(defn produce-message
+  [session producer]
+  (let [message (.createTextMessage session "message body")]
+    (.send producer message)
+    message))
+
+(defn do-participant
+  "Synchronously participates, collecting messages, until the deadline"
+  [participate-fn deadline]
+  (loop [messages (list)]
+    (let [new-messages (cons (participate-fn) messages)]
+      (java.lang.Thread/sleep (rand-int 400))
+      (if (> (java.lang.System/currentTimeMillis) deadline)
+        new-messages
+        (recur new-messages))))) 
+
+
 (defn -main [& args]
   (println "Starting...")
   (delete-file-recursively (get-system-property "QPID_WORK") true)
@@ -35,24 +59,22 @@
       ; TODO how to set up auto-closeable resources?
       
       (let [connection-factory (new AMQConnectionFactory "amqp://guest:guest@clientid/?brokerlist='tcp://localhost:5672'")
-            connection (.createConnection connection-factory "guest" "guest")
-            session (.createSession connection false Session/AUTO_ACKNOWLEDGE)
-            queue (.createQueue session queue-name)]
+            connection (.createConnection connection-factory)]
         (try
           (create-queue)
-          (def producer-future (future
-                                 (let [producer (.createProducer session queue)
-                                       message (.createTextMessage session "hello world")]
-                                   (.send producer message)
-                                   (println "Message sent inside future")
-                                   message)))
-          (def consumer-future (future
-                                 (let [consumer (.createConsumer session queue)]
-                                   (.start connection)
-                                   (.receive consumer 1000))))
-          (println "Producer produced: " (.getJMSMessageID @producer-future))
-          (println "Consumer received: " (.getJMSMessageID @consumer-future))
+          (.start connection)
+          
+          (let [deadline (+ (java.lang.System/currentTimeMillis) 3000)]
+            (doseq [producer-future (take 3
+              (repeatedly
+                #(future
+                  (let [session (.createSession connection false Session/AUTO_ACKNOWLEDGE)
+                        producer (.createProducer session (.createQueue session queue-name))]
+                    (do-participant (fn [] (produce-message session producer)) deadline)))))]
+              (println @producer-future)))
+            
           (finally (delete-queue))))
+      
       (finally
         (println "About to shut down broker")
         (.shutdown broker)))))
