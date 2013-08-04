2b4436719791231ee17125c919c1a416bb16e6fb
Phil Harvey <phil@philharveyonline.com>
2013-08-04 19:15:22 +0100

    Now printing all the produced messages, and starting to consume them too

--- src/org/apache/qpid/jmschaos/core.clj
+++ src/org/apache/qpid/jmschaos/core.clj
@@ -3,11 +3,16 @@
     (org.apache.qpid.server Broker BrokerOptions)
     (org.apache.qpid.client AMQConnectionFactory)
     (javax.jms Connection ConnectionFactory Session Message))
-  (:require [clj-http.client :as client])
-  (:use [clojure.contrib.java-utils ]))
+  
+  (:require [clj-http.client :as client]
+            [clojure.contrib.java-utils :as java-utils])
+  
+  (:use clojure.tools.logging))
 
 
 (def duration 10000)
+
+(def broker (new Broker))
 
 (def connection-factory (new AMQConnectionFactory "amqp://guest:guest@clientid/?brokerlist='tcp://localhost:5672'"))
   
@@ -18,14 +23,14 @@
 
 
 (defn create-queue []
-  (println "About to create queue")
+  (info "About to create queue")
   (client/put queue-url {:body "{\"durable\":true}"} )
   (client/put binding-url {:body "{}"} ))
 
 (defn delete-queue []
   (client/delete binding-url)
   (client/delete queue-url)
-  (println "Deleted queue"))
+  (info "Deleted queue"))
 
 (defn do-for-at-most
   "Synchronously participates until at most duration ms have passed, returning sent/received messages"
@@ -41,31 +46,46 @@
           new-messages
           (recur new-messages))))))
 
-(defn run-consumer [session queue]
-  (future
-    (let [consumer (.createConsumer session queue)]
-      (.receive consumer 1000))))
 
 (defn produce-message
+  "Produce one message and return its JMSMessageID"
   [session producer]
   (let [message (.createTextMessage session "message body")]
     (.send producer message)
     (.getJMSMessageID message)))
 
-(defn produce [connection] 
+
+(defn produce [connection]
+  "Creates a session, produces some messages, then closes the session"
+  
   (Thread/sleep (rand-int (/ duration 10)))
   (with-open [session (.createSession connection false Session/AUTO_ACKNOWLEDGE)]
-      (do-for-at-most
-        duration 
-        (fn [] 
-          (with-open [producer (.createProducer session (.createQueue session queue-name))]
-            (do-for-at-most
-              (/ duration 3)
-              (fn [] (produce-message session producer))))))))
+    (do-for-at-most
+      duration 
+      (fn [] 
+        (with-open [producer (.createProducer session (.createQueue session queue-name))]
+          (do-for-at-most
+            (/ duration 3)
+            (fn [] (produce-message session producer))))))))
+
+(defn consume [connection]
+  "Creates a session, consumes some messages, then closes the session (TODO reduced duplication)"
+  
+  (Thread/sleep (rand-int (/ duration 10)))
+  (with-open [session (.createSession connection false Session/AUTO_ACKNOWLEDGE)]
+    (do-for-at-most
+      duration 
+      (fn [] 
+        (with-open [consumer (.createConsumer session (.createQueue session queue-name))]
+          (do-for-at-most
+            (/ duration 3)
+            (fn [] (.receive consumer duration))))))))
+
+
 
 (defn -main [& args]
-  (println "Starting...")
-  (delete-file-recursively (get-system-property "QPID_WORK") true)
+  (info "Starting...")
+  (java-utils/delete-file-recursively (java-utils/get-system-property "QPID_WORK") true)
   
   (let [options (new BrokerOptions)
         broker (new Broker)]
@@ -77,15 +97,30 @@
       (with-open [connection (.createConnection connection-factory)]
         (.start connection)
         
+        (let [producer-futures (doall (take 5 (repeatedly (fn [] (future (produce connection))))))
+              consumer-futures (doall (take 5 (repeatedly (fn [] (future (consume connection))))))]
+          (let [messages-produced (flatten (map deref producer-futures))]
+            (info "Messages produced: " messages-produced)))
+        
         (time
         (doseq [producer-future (doall (take 5 (repeatedly
                                                  (fn [] (future (produce connection))))))]
-          (println "Summary:"
+          (info "Summary:"
                    (count @producer-future)
                    "producers produced"
                    (count (flatten @producer-future)) "messages"))))
       
       (finally
         (delete-queue)
-        (println "About to shut down broker")
+        (info "About to shut down broker")
         (.shutdown broker)))))
+
+(defn set-up-repl []
+  (System/setProperty "QPID_WORK" "target/work")
+  (System/setProperty "QPID_HOME" "qpd-home")
+  (let [options (new BrokerOptions)]
+    (.setInitialConfigurationLocation options (.toExternalForm (clojure.java.io/resource "config.json")))
+    (.startup broker options)
+    (create-queue)
+    (def connection (.createConnection connection-factory))))
+  
